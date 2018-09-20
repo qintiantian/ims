1769ca6773265d683c81013ca04478fb57ac9928
qintiantian <qin_tiantianliling@163.com>
2018-09-20 18:54:28 +0800

    获取消息图片列表 修改

--- src/main/java/com/konglk/mappers/MsgDao.java
+++ src/main/java/com/konglk/mappers/MsgDao.java
@@ -21,4 +21,3 @@ public interface MsgDao {
     @Select("select m.content from ims_msg m where ((m.send_id=#{sendId} and m.dest_id=#{destId}) or (m.send_id = #{destId} and m.dest_id=#{sendId})) and m.msg_type = 3 order by m.ts")
     List<String> selectImagesById(@Param("sendId") String sendId, @Param("destId") String destId);
 }
-src/main/java/com/konglk/controller/UserController.java
